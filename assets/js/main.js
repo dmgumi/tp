524473edd766483216a1151850a4885c1750cd6b
Gaétan <frion.gaetan@gmail.com>
2022-03-07 11:00:36 +0100

    version finale

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,12 @@
 $("form").submit((e) => {
       e.preventDefault();
-
-      product = $("#product").val();
-      brand = $("#brand").val();
-      model = $("#model").val();
-      ref = $("#ref").val();
-      spec = $("#spec").val();
-
-      $("textarea").val(`${product + " " + brand}
-${brand + " " + model}
-${brand + " " + ref}
-${product + " " + brand + " " + spec}`);
-
-      $("#product").val("");
-      $("#brand").val("");
-      $("#model").val("");
-      $("#ref").val("");
-      $("#spec").val("");
-});
-
-$("label").last().css("visibility", "hidden");
-
-$("#reset").click(function () {
-      $("textarea").val("");
+      $("textarea").val(`${$("#product").val() + " " + $("#brand").val()}
+${$("#brand").val() + " " + $("#model").val()}
+${$("#brand").val() + " " + $("#ref").val()}
+${$("#product").val() + " " + $("#brand").val() + " " + $("#spec").val()}`);
 });
 
 $("#copy").click(function () {
+      $("textarea").val().select()
       navigator.clipboard.writeText($("textarea").val());
 });
